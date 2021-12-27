7478578cff3edda11d72693369b876335d0b1ce6
Hari Ram J P <hariramjp777@gmail.com>
2021-12-27 21:13:17 +0530

    Add colors.h

---
+++ colors.h
@@ -0,0 +1,19 @@
+#ifndef COLORS_H
+#define COLORS_H
+
+#define RESET           "\033[0m"
+#define F_GRAY          "\033[1;30m"
+#define B_GRAY          "\033[1;30;47m"
+#define F_BLUE          "\033[0;34m"
+#define F_GREEN         "\033[0;32m"
+#define F_RED           "\033[1;31m"
+#define F_D_BLUE        "\033[1;34m"
+#define F_PINK          "\033[0;35m"
+#define F_CYAN          "\033[0;36m"
+#define F_L_GRAY        "\033[0;37m"
+#define F_YELLOW        "\033[0;33m"
+#define B_RED_F_BLACK   "\033[0;30;41m"
+#define B_YELL_F_BLACK	"\033[7;49;33m"
+#define F_B_PURPLE      "\e[1;35m"
+
+#endif
